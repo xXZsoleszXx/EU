ab5482f20054b3aaaee6ac96886a82c5ee5c15a0
zsolesz <aktakukac56@gmail.com>
2021-09-20 09:40:45 +0200

    Add project files.

---
+++ EU/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EU
+{
+  class Program
+  {
+    
+    static void MasodikFeladat() 
+    {
+      string[] adatok = File.ReadAllLines("EUcsatlakozas.txt");
+      foreach (var i in adatok)
+      {
+        Console.WriteLine(i);
+      }
+     
+
+     
+    }
+    static void Main(string[] args)
+    {
+      MasodikFeladat();
+
+      Console.ReadKey();
+    }
+  }
+}
